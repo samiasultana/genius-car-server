0c5f27c9cfb69272ae29d9721c1a054c69868774
unknown <samia361@gmail.com>
2021-11-18 00:37:14 +0600

    post-api

--- index.js
+++ index.js
@@ -15,7 +15,23 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run() {
   try {
     await client.connect();
-    console.log("connected to db");
+    // console.log("connected to db");
+    const database = client.db("carMechanic");
+    const serviceCollection = database.collection("Collection of service");
+
+    // POST API
+    app.post("/services", async (req, res) => {
+      const service = {
+        "name": "ENGINE DIAGNOSTIC",
+        "price": "300",
+        "description": "Lorem ipsum dolor sit amet, consectetu radipisi cing elitBeatae autem aperiam nequ quaera molestias voluptatibus harum ametipsa.",
+        "img": "https://i.ibb.co/dGDkr4v/1.jpg"
+      }
+
+      const result = await serviceCollection.insertOne(service);
+      console.log(result);
+
+    })
   }
   finally {
     // await client.close();
