7016fc9615db58adc9e513f598ae70f5a9e485f0
unknown <samia361@gmail.com>
2021-11-18 00:17:32 +0600

    db-connected

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // npm i express mongodb cors dotenv initially so that all necessary modules are present in the server. now go to the express and copy all from hello world
 const express = require('express');
 const { MongoClient } = require('mongodb');
-require('dotenv').config() //install dotenv and use here. it will help to keep mongo password secret from github. Then create a .env file and paste NAME=VALUE from dotenv site. Password and host value should be taken from mongodb atlas>security>database access>add new. Then keep the const uri inside ``
+require('dotenv').config(); //install dotenv and use here. it will help to keep mongo password secret from github. Then create a .env file and paste NAME=VALUE from dotenv site. Password and user value should be taken from mongodb atlas>security>database access>add new. Then keep the const uri inside ``
 
 var cors = require('cors');
 const app = express();
@@ -9,15 +9,16 @@ app.use(cors());
 const port = process.env.PORT || 5000;
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zyodo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-// console.log(uri);
+console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 async function run() {
   try {
-
+    await client.connect();
+    console.log("connected to db");
   }
   finally {
-
+    // await client.close();
   }
 }
 run().catch(console.dir);
